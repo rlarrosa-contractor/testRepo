3014a01ee148b43862123c1fd406aa6d695ca133
Rodrigo Larrosa <rodrigo.larrosa@globant.com>
2013-12-30 12:49:54 -0200

    Add TestViewController

---
+++ testGithub/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  testGithub
+//
+//  Created by Rodrigo Larrosa on 12/30/13.
+//  Copyright (c) 2013 Rodrigo Larrosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
---
+++ testGithub/TestViewController.m
@@ -0,0 +1,38 @@
+//
+//  TestViewController.m
+//  testGithub
+//
+//  Created by Rodrigo Larrosa on 12/30/13.
+//  Copyright (c) 2013 Rodrigo Larrosa. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
